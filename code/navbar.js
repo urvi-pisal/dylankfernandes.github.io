2f9b3c5ef235350b970fc5c8e5ee9f8f0ce9ccda
Developer5262 <dylan.fernandes00@gmail.com>
2017-08-08 15:02:39 -0400

    added homepage responsiveness

--- code/navbar.js
+++
@@ -1,8 +0,0 @@
-function navbar() {
-    var x = document.getElementById("nav");
-    if (x.className === "navbar") {
-        x.className += " responsive";
-    } else {
-        x.className = "navbar";
-    }
-}
